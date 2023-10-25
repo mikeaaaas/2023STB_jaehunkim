3c22760a08ac1d125bafcb06eea6fd6aa7ad49eb
김재훈 <jaehunkim0215@naver.com>
2023-10-25 17:30:31 +0900

    re

--- 08. Localdata_seoul_food_2023.R
+++ 08. Localdata_seoul_food_2023.R
@@ -1,10 +1,12 @@
 #실습에 필요한 packages를 라이브러리에 등록
 library(dplyr)
 library(ggplot2)
+install.packages("dplyr")
+install.packages("ggplot2")
 
 #데이터를 새객체 foodshop으로 불러오기
 #비어있는 셀은 결측치 처리/파라미터 문자형으로 변환
-foodshop <- read.csv("seoul_food_2023.csv", na="", stringsAsFactors = F)
+foodshop <- read.csv("seoul_food_2023.csv.csv", na="", stringsAsFactors = F)
 
 #데이터 구조 확인
 str(foodshop)
